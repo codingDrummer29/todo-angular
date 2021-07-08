24a2dbade84115892d6586a003b65785b5a10884
Anubhab <mukerji29anuvb@gmail.com>
2021-07-08 21:50:05 +0530

    adding & deleting items functionality added to inside app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './todo';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // title = 'angular-crashy';
   title = 'Angular Crash Course';
-  date = new Date();
-  name = 'Anubhab Here';
 
-  constructor() {
-    console.log('Constructor called');
-    this.doSomething('anubhab');
+  todoValue!: string;
+  list: Todo[] = [];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = '';
   }
 
-  doSomething(val: string): void {
-    val = 'Do Something is working';
+  addItem() {
+    if (this.todoValue !== '') {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false,
+      };
+      // now new item is created so we need to store that
+      this.list.push(newItem);
+    }
+    // now as the data is stored, clear the box
+    this.todoValue = '';
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
   }
 }
