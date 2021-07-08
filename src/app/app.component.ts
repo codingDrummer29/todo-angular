062c2b10a798cf04da3b2cdeb45f50f1ce04b008
Anubhab <mukerji29anuvb@gmail.com>
2021-07-08 20:34:37 +0530

    Todo App UI created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-crashy';
+  // title = 'angular-crashy';
+  title = 'Angular Crash Course';
+  date = new Date();
+  name = 'Anubhab Here';
+
+  constructor() {
+    console.log('Constructor called');
+    this.doSomething('anubhab');
+  }
+
+  doSomething(val: string): void {
+    val = 'Do Something is working';
+  }
 }
